9807e019251617d7873012cab9691854ad882805
manishraj <wingsmanish11@gmail.com>
2025-08-28 23:28:03 +0530

    final update

--- next.config.ts
+++ next.config.ts
@@ -1,25 +1,34 @@
-import type { NextConfig } from "next";
+// import type { NextConfig } from "next";
 
-// const nextConfig: NextConfig = {
-//   /* config options here */
+// // const nextConfig: NextConfig = {
+// //   /* config options here */
+// // };
+
+// // next.config.js
+// import path from "path";
+
+// module.exports = {
+//   webpack: (config) => {
+//     // Ignore system folders like Application Data
+//     config.watchOptions = {
+//       ignored: [
+//         "**/node_modules",
+//         "**/.git",
+//         "**/Application Data/**",
+//       ],
+//     };
+//     return config;
+//   },
 // };
-
-// next.config.js
-const path = require("path");
-
-module.exports = {
-  webpack: (config) => {
-    // Ignore system folders like Application Data
-    config.watchOptions = {
-      ignored: [
-        "**/node_modules",
-        "**/.git",
-        "**/Application Data/**",
-      ],
-    };
-    return config;
-  },
-};
 
 
 // export default nextConfig;
+
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  experimental: {
+    optimizeCss: false,
+  },
+};
+
+module.exports = nextConfig;

2c6a010a17fe848e6f6d95f41821a0e042128197
manishraj <wingsmanish11@gmail.com>
2025-08-29 00:32:35 +0530

    ..

--- next.config.ts
+++ next.config.ts
@@ -10,7 +10,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   experimental: {
-    optimizeCss: true, // LightningCSS disable
+    optimizeCss: false, // LightningCSS disable
   },
 };
 
